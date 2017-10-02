1b8dd31047f3776f472111f074a168318a34664f
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-10-02 07:28:58 +0100

    Add clojure.

---
+++ codejam-clj/src/codejam_clj/nextnumber.clj
@@ -0,0 +1,20 @@
+
+(ns codejam-clj.nextnumber)
+
+(use 'clojure.contrib.combinatorics)
+
+(defn permute-number [n]
+  (map #(read-string (.replaceFirst (apply str %) "^0+" ""))
+       (permutations (seq (str n)))))
+
+(defn next-number [n m]
+  (first (filter #(> % m) (sort <  (permute-number n)))))
+
+(defn find-next-number [n]
+  (let [i (next-number n n)]
+    (if i
+      i
+      (next-number (* n 10) n))))
+
+(doseq [i (range 1 (+ (read) 1))]
+  (println "Case" (str "#" i ":") (find-next-number (read))))
