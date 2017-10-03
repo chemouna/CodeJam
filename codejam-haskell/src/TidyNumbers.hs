0d08d4e6bd1e0963d2c10b19e776978e9584f220
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-10-03 22:53:16 +0100

    Attempt 2.

--- codejam-haskell/src/TidyNumbers.hs
+++ codejam-haskell/src/TidyNumbers.hs
@@ -3,14 +3,13 @@
 module TidyNumbers where
 
 import Control.Monad (mapM_)
-import Data.List (sort)
+import Data.List (filter, map, permutations, sort)
 
 reduceToTidy :: String -> String
 reduceToTidy [] = []
 reduceToTidy (a : as) =
   if replicate (length as) a > as then pred a : replicate (length as) '9'
   else a : reduceToTidy as
-
 
 solveInput = do
   s <- reduceToTidy <$> getLine
@@ -33,11 +32,27 @@ largestTidy = read . td [] . reverse . show
         td as@(a:_) (x:xs) | x <= a    = td (x:as) xs
                            | otherwise = td ((pred x):('9' <$ as)) xs
 
-{--
-main' = do t <- readLn :: IO Int
-            flip  mapM_ [1..t] $ \c -> do
-            n <- readLn
-            putStr $ "Case #" ++ show c ++ ": "
-            print $ largestTidy n
+main2 = do
+  t <- readLn :: IO Int
+  flip mapM_ [1 .. t] $ \c -> do
+       n <- readLn
+       putStr $ "Case #" ++ show c ++ ": "
+       print $ largestTidy n
 
+
+isTidy2 :: Int -> Bool
+isTidy2 n = sort sn == sn
+  where sn = show n
+
+-- latestTidyNumber :: Integer -> Integer
+latestTidyNumber n = filter (\x -> x < n && (isTidy2 x))  (map (\x -> read x :: Integer) (permutations (show n)))
+
+{--
+main3 = do
+  x <- readLn :: IO Int
+  flip mapM [1 .. x] $ \c -> do
+    n <- readLn
+    putStr $ "Case #" ++ show c ++ ": "
+    print $ latestTidyNumber n
 --}
+
