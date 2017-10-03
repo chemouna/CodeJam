55ad889aeaea6427ee84b237418915571b1f122c
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-10-03 07:45:00 +0100

    Start solution 2 to Tidy numbers.

--- codejam-haskell/src/TidyNumbers.hs
+++ codejam-haskell/src/TidyNumbers.hs
@@ -1,5 +1,9 @@
+{-# LANGUAGE TupleSections #-}
 
 module TidyNumbers where
+
+import Control.Monad (mapM_)
+import Data.List (sort)
 
 reduceToTidy :: String -> String
 reduceToTidy [] = []
@@ -18,3 +22,22 @@ main = do
     result <- solveInput
     putStr ("Case #" ++ show c ++ ": " ++ result ++ "\n")
 
+
+isTidy :: Integer -> Bool
+isTidy n = let str = show n in str == sort str
+
+largestTidy :: Integer -> Integer
+largestTidy = read . td [] . reverse . show
+  where td acc [] = acc
+        td [] (x:xs) = td [x] xs
+        td as@(a:_) (x:xs) | x <= a    = td (x:as) xs
+                           | otherwise = td ((pred x):('9' <$ as)) xs
+
+{--
+main' = do t <- readLn :: IO Int
+            flip  mapM_ [1..t] $ \c -> do
+            n <- readLn
+            putStr $ "Case #" ++ show c ++ ": "
+            print $ largestTidy n
+
+--}
