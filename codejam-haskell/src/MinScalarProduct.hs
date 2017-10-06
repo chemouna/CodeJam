5ca0387eefc4d1e4fd421ad8da4e577e28bea846
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-10-06 05:34:57 +0100

    Add input parsing.

--- codejam-haskell/src/MinScalarProduct.hs
+++ codejam-haskell/src/MinScalarProduct.hs
@@ -1,10 +1,12 @@
 module MinScalarProduct where
 
+import Control.Applicative
+import Control.Monad
 import Data.List
 
 -- TODO: do the same in Idris but with the constraint on length of the vectors to be equal
 
-minScalarProduct :: [Int] -> [Int] -> Int
+minScalarProduct :: (Num a, Eq a, Ord a) => [a] -> [a] -> a
 minScalarProduct v1 v2 = minimum $ map (uncurry scalarProduct) $ permCombinations pv1 pv2
   where
     scalarProduct xs ys = sum $ zipWith (\x y -> x * y) xs ys
@@ -12,3 +14,15 @@ minScalarProduct v1 v2 = minimum $ map (uncurry scalarProduct) $ permCombination
     pv1 = filter (\x -> not (x == v1)) (permutations v1)
     pv2 =  filter (\x -> not (x == v2)) (permutations v2)
 
+
+one_case :: Int -> IO ()
+one_case i = do
+  n <- liftM read getLine :: IO Int
+  a <- liftM (map read . words) getLine
+  b <- liftM (map read . words) getLine
+  putStrLn ("Case #" ++ show i ++ ": " ++ show (minScalarProduct a b))
+
+main :: IO ()
+main = do
+  t <- liftM read getLine
+  sequence_ (map one_case [1 .. t])
