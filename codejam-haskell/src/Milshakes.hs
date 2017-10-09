92512a6ba68ad50a37cffa60b40b37c255dc9bf5
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-10-09 07:49:30 +0100

    Finish orchestration and setting up for Milshakes.hs

--- codejam-haskell/src/Milshakes.hs
+++ codejam-haskell/src/Milshakes.hs
@@ -1,3 +1,34 @@
-
 module Milshakes where
 
+import Control.Monad
+import Data.Maybe
+import Data.Array
+
+type Constraint = [(Int, Bool)] -- list of the flavors customers like and whether they are malted or not
+
+-- get the input for a customer
+get_customer :: IO Constraint
+get_customer =
+  let pc acc [] = acc
+      pc acc (i:b:cs) = pc ((i - 1, b == 1) : acc) cs
+  in
+  do (_ : cs) <- liftM (map read . words) getLine
+     return (pc [] cs)
+
+show_arr :: Array Int Bool -> String
+show_arr a = unwords [if b then "1" else "0" | b <- elems a]
+
+solve :: Int -> [Constraint] -> Maybe (Array Int Bool)
+solve = undefined
+
+one_case :: Int -> IO ()
+one_case i = do
+  n <- liftM read getLine
+  m <- liftM read getLine
+  cs <- sequence (replicate m get_customer)
+  putStrLn ("Case #" ++ show i ++ ": " ++ fromMaybe "NOTHING" (liftM show_arr (solve n cs)))
+
+main :: IO ()
+main = do
+  c <-liftM read getLine
+  sequence_ (map one_case [1..c])
