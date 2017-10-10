8936385dc0fb5b4e17b632826e2dc72b2e96dfa7
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-10-10 07:26:03 +0100

    Improve Milshakes solution.

--- codejam-kotlin/src/com/mounacheikhna/cj2008/Milshakes.kt
+++ codejam-kotlin/src/com/mounacheikhna/cj2008/Milshakes.kt
@@ -75,41 +75,42 @@ class Milshakes {
 
     private fun solveCase(m: Int, n: Int, customers: List<CustomerInput>): BooleanArray {
         val a = array2dOfBoolean(m, n) // ? array of customers  milshakes flavors where a[i][j] is true when customer i is assigned a unmalted milshake j
-        val l = IntArray(m)
-        val b = IntArray(m) // contains whether a customer has a malted milshake
-        Arrays.fill(b, -1) // so if b[i] is != -1 => customer i has a malted milshake of index b[i]
+        val countLikesUnmalted = IntArray(m)
+        val likesMalted = IntArray(m) // contains whether a customer has a malted milshake
+        Arrays.fill(likesMalted, -1) // so if b[i] is != -1 => customer i has a malted milshake of index b[i]
         val st = ArrayList<Int>()
         val ans = BooleanArray(n)
 
         customers.forEachIndexed { i, (_, likes) ->
             likes.forEach { (u, v) ->
                 if (v == 1) { // malted
-                    b[i] = u
+                    likesMalted[i] = u
                 } else {
                     if (!a[i][u]) { // if customer i is not assigned already the milshake he likes and since he wants unmalted flavor we increase nb of unmalted flavor for customer i
-                        l[i]++ //increased nb of unmalted batches
+                        countLikesUnmalted[i]++ //increased nb of unmalted batches
                     }
                     a[i][u] = true
                 }
             }
-            if (l[i] == 0) {
+            if (countLikesUnmalted[i] == 0) {
                 st.add(i)
             }
         }
 
         while (st.size > 0) {
             val i = st.removeAt(st.size - 1)
-            if (b[i] == -1) {
-                //TODO : make an inner function and make it return false in this case then go to next case if false is returned
-                return ans
+            if (likesMalted[i] == -1) {
+                // they must be unsatisfied because none of their prefered
+                // MALTED flavours have been made
+                return ans //? not sure of this at all
             }
-            ans[b[i]] = true
+            ans[likesMalted[i]] = true
 
             for (j in 0 until m) {
-                if (a[j][b[i]]) {
-                    a[j][b[i]] = false //!Ъ //?
-                    l[j]--
-                    if (l[j] == 0) {
+                if (a[j][likesMalted[i]]) {
+                    a[j][likesMalted[i]] = false //!Ъ //?
+                    countLikesUnmalted[j]--
+                    if (countLikesUnmalted[j] == 0) {
                         st.add(j)
                     }
                 }
